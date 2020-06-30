6e84e1f2a95ed9d293146ee5eedbb2e488a67129
Tomoki Yamauchi <tmk-0905@ruri.waseda.jp>
2020-06-30 12:13:27 +0900

    交差エントロピー誤差の計算メソッドを実装

--- common/functions.py
+++ common/functions.py
@@ -18,3 +18,15 @@ def softmax(x):
 
 def identity_function(x):
     return x
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    # 教師データが one-hot-vector の場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
