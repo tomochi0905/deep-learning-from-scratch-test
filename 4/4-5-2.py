99d7f18f9b53c0267e4729cc7e707e3affa79b6a
Tomoki Yamauchi <tmk-0905@ruri.waseda.jp>
2020-07-01 22:40:34 +0900

    「4.5.2 ミニバッチ学習の実装」のコード実装

---
+++ 4/4-5-2.py
@@ -0,0 +1,45 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+# ハイパーパラメータ
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    print(str(i))
+    
+    # ミニバッチの取得
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 勾配の計算
+    grad = network.numerical_gradient(x_batch, t_batch)
+    # grad = network.gradient(x_batch, t_batch) # 高速版
+
+    # パラメータの更新
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    # 学習過程の記録
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+# グラフの描画
+x = np.arange(len(train_loss_list))
+plt.plot(x, train_loss_list)
+plt.xlabel("iteration")
+plt.ylabel("loss")
+plt.show()
