0e971eca44ff6a3f3e2096eb6fdf274918fed59f
Tomoki Yamauchi <tmk-0905@ruri.waseda.jp>
2020-07-04 13:22:03 +0900

    「5.6.2 バッチ版 Affine レイヤ」のコード実装

--- common/layers.py
+++ common/layers.py
@@ -31,3 +31,31 @@ class Sigmoid:
         dx = dout * self.out * (1.0 - self.out)
 
         return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.original_x_shape = None
+        # 重み・バイアスデータの微分
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        # テンソル対応
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+        
+        out = np.dot(self.x, self.W) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        # 入力データの形状に戻す（テンソル対応）
+        dx = dx.reshape(*self.original_x_shape)
+        return dx
