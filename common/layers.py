fb794e70c449628bfe3db4b170407be47dc91e0c
Tomoki Yamauchi <tmk-0905@ruri.waseda.jp>
2020-07-04 12:51:30 +0900

    「5.5.2 Sigmoid レイヤ」のコード実装

--- common/layers.py
+++ common/layers.py
@@ -16,3 +16,18 @@ class Relu:
         dx = dout
 
         return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+    
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+    
+    def backward(self, dout):
+        dx = dout * self.out * (1.0 - self.out)
+
+        return dx
