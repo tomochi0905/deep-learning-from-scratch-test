2a462f2060bbbdceafa515b6da620aaeacab0194
Tomoki Yamauchi <tmk-0905@ruri.waseda.jp>
2020-07-04 12:47:41 +0900

    「5.5.1 ReLU レイヤ」の実装で forward メソッドの引数を修正

--- common/layers.py
+++ common/layers.py
@@ -4,7 +4,7 @@ class Relu:
     def __init__(self):
         self.mask = None
     
-    def forward(self, x, y):
+    def forward(self, x):
         self.mask = (x <= 0)
         out = x.copy()
         out[self.mask] = 0
