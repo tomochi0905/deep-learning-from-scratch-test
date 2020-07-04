12fce5c1bac323ab3d308883799b841ea38fce3d
Tomoki Yamauchi <tmk-0905@ruri.waseda.jp>
2020-07-04 19:13:47 +0900

    softmax 関数の計算にオプションを追加

--- common/functions.py
+++ common/functions.py
@@ -11,9 +11,9 @@ def relu(x):
     return np.maximum(0, x)
 
 def softmax(x):
-    c = np.max(x)
+    c = np.max(x, axis=-1, keepdims=True)
     exp_x = np.exp(x - c)
-    sum_exp_x = np.sum(exp_x)
+    sum_exp_x = np.sum(exp_x, axis=-1, keepdims=True)
     return exp_x / sum_exp_x
 
 def identity_function(x):
