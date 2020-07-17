32f81f10d9ea07bcf31ace3a75db1368444b3dd1
Tomoki Yamauchi <tmk-0905@ruri.waseda.jp>
2020-07-17 16:07:26 +0900

    「7.4.3 Convolution レイヤの実装」のコード実装

---
+++ 7/7-4-3.py
@@ -0,0 +1,12 @@
+import os, sys
+sys.path.append(os.pardir)
+import numpy as np
+from common.util import im2col
+
+x1 = np.random.rand(1, 3, 7, 7)
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape) # (9, 75)
+
+x2 = np.random.rand(10, 3, 7, 7) # 10 個のデータ
+col2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape) # (90, 75)
